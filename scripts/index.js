9975716781879f900c6ffb6ad2f5fffe315a4ff8
tweaki-io <tweaki.io@gmail.com>
2025-12-19 18:27:27 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-const scrape = require('website-scraper');
-const PuppeteerPlugin = require('website-scraper-puppeteer');
+const scrape = require('website-scraper').default;
+const PuppeteerPlugin = require('website-scraper-puppeteer').default;
 const fs = require('fs');
 const path = require('path');
 
